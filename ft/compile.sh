c0bdfd5f160ed7a43e1341b68de22229fed3895a
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2022-03-09 18:29:36 +0300

    Add preliminary jettons code

---
+++ ft/compile.sh
@@ -0,0 +1,4 @@
+func -SPA ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc distributed-token.fc -o ./build/ft.fif
+echo '"build/ft.fif" include 2 boc+>B "build/ft_code.boc" B>file' | fift -s
+func -SPA ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc wrapped_ton.fc -o ./build/wton.fif
+
