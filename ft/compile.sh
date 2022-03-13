9fb70ffe7538bb624ce5f0368ed47e0df8343255
tolya-yanot <1449561+tolya-yanot@users.noreply.github.com>
2022-03-14 01:49:38 +0300

    temporary rollback const; token_wallet_code stores in ref in minter because tonweb; fixes;

--- ft/compile.sh
+++ ft/compile.sh
@@ -1,5 +1,6 @@
-func -SPA ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc distributed-token.fc -o ./build/ft.fif
+func -SPA -o ./build/ft.fif ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc distributed-token.fc
 echo '"build/ft.fif" include 2 boc+>B "build/ft_code.boc" B>file' | fift -s
-func -SPA ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc wrapped_ton.fc -o ./build/wton.fif
-func -SPA ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc minter.fc -o ./build/jetton-minter.fif
+func -SPA -o ./build/wton.fif ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc wrapped_ton.fc
+func -SPA -o ./build/jetton-minter.fif ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc minter.fc
 
+fift -s build/print-hex.fif
