8b9b60db9c441a3403023240f0a0d73279b29d8f
tolya-yanot <1449561+tolya-yanot@users.noreply.github.com>
2022-03-24 17:29:54 +0300

    rename files

--- ft/compile.sh
+++ ft/compile.sh
@@ -1,6 +1,5 @@
-func -SPA -o ./build/ft.fif ../stdlib.fc op_codes.fc jetton_wallet_utils.fc distributed-token.fc
-echo '"build/ft.fif" include 2 boc+>B "build/ft_code.boc" B>file' | fift -s
-func -SPA -o ./build/wton.fif ../stdlib.fc op_codes.fc jetton_wallet_utils.fc wrapped_ton.fc
-func -SPA -o ./build/jetton-minter.fif ../stdlib.fc op_codes.fc jetton_wallet_utils.fc minter.fc
+func -SPA -o ./build/jetton-wallet.fif ../stdlib.fc op-codes.fc jetton-wallet-utils.fc jetton-wallet.fc
+echo '"build/jetton-wallet.fif" include 2 boc+>B "build/jetton-wallet.boc" B>file' | fift -s
+func -SPA -o ./build/jetton-minter.fif ../stdlib.fc op-codes.fc jetton-wallet-utils.fc jetton-minter.fc
 
 fift -s build/print-hex.fif
