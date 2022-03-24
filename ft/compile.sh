f12569ffa4d615d3d48ad691cd545a6f6defc091
tolya-yanot <1449561+tolya-yanot@users.noreply.github.com>
2022-03-24 17:22:43 +0300

    move some common functions to stdlib

--- ft/compile.sh
+++ ft/compile.sh
@@ -1,6 +1,6 @@
-func -SPA -o ./build/ft.fif ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc distributed-token.fc
+func -SPA -o ./build/ft.fif ../stdlib.fc op_codes.fc jetton_wallet_utils.fc distributed-token.fc
 echo '"build/ft.fif" include 2 boc+>B "build/ft_code.boc" B>file' | fift -s
-func -SPA -o ./build/wton.fif ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc wrapped_ton.fc
-func -SPA -o ./build/jetton-minter.fif ../nft/stdlib.fc op_codes.fc jetton_wallet_utils.fc minter.fc
+func -SPA -o ./build/wton.fif ../stdlib.fc op_codes.fc jetton_wallet_utils.fc wrapped_ton.fc
+func -SPA -o ./build/jetton-minter.fif ../stdlib.fc op_codes.fc jetton_wallet_utils.fc minter.fc
 
 fift -s build/print-hex.fif
