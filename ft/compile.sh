eb56ab3ff245905b4bb1217145fe5bf2f32313bf
tolya-yanot <1449561+tolya-yanot@users.noreply.github.com>
2022-03-24 17:44:40 +0300

    jetton: add params.fc

--- ft/compile.sh
+++ ft/compile.sh
@@ -1,5 +1,5 @@
-func -SPA -o ./build/jetton-wallet.fif ../stdlib.fc op-codes.fc jetton-wallet-utils.fc jetton-wallet.fc
+func -SPA -o ./build/jetton-wallet.fif ../stdlib.fc params.fc op-codes.fc jetton-wallet-utils.fc jetton-wallet.fc
 echo '"build/jetton-wallet.fif" include 2 boc+>B "build/jetton-wallet.boc" B>file' | fift -s
-func -SPA -o ./build/jetton-minter.fif ../stdlib.fc op-codes.fc jetton-wallet-utils.fc jetton-minter.fc
+func -SPA -o ./build/jetton-minter.fif ../stdlib.fc params.fc op-codes.fc jetton-wallet-utils.fc jetton-minter.fc
 
 fift -s build/print-hex.fif
